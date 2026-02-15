7b7d5511d84ca9e432560d6b17d592411031059b
Kapil Rahangdale <kapilrhngdle2211@gmail.com>
2026-02-15 18:03:30 +0530

    Final commit made logo fixed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,22 @@
-import logo from "../assets/logo.png";
+import logo from "../assets/TM_logo.png";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen bg-gradient-to-br 
       from-emerald-100 via-white to-rose-100 relative overflow-hidden">
 
       {/* Header */}
       <header className="absolute top-6 left-10 z-20 flex items-center gap-3">
-
-        <img src={logo} alt="logo" className="w-10 h-10 drop-shadow-lg" />
-
-        <h1
-          className="text-3xl font-extrabold 
-          bg-gradient-to-r from-emerald-500 via-teal-400 to-rose-500
-          bg-clip-text text-transparent
-          drop-shadow-[0_4px_6px_rgba(0,0,0,0.15)]
-          tracking-wide"
-        >
-          Task Manager
-        </h1>
+        <img
+          src={logo}
+          alt="TaskTracker Logo"
+          className="h-32 md:h-36 w-auto object-contain drop-shadow-2xl"
+         // className="h-14 w-auto object-contain drop-shadow-xl"
+        />
       </header>
 
       {children}
